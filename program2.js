02ae675d76dce40676a8c9b863d934de81c71808
krisnranjan27 <krisnranjan211051cse@sdbc.ac.in>
2024-07-26 13:21:48 +0530

    Update program2.js
    test 2 passed

--- program2.js
+++ program2.js
@@ -1,7 +1,43 @@
 const decodeTheRing = function (s, p) {
+  // write your code here
 
-    // write your code here
-
-  };
   
-  module.exports = decodeTheRing;
+  function match(s, p) {
+    let sIdx = 0,
+      pIdx = 0;
+    let starIdx = -1,
+      sTmpIdx = -1;
+
+    while (sIdx < s.length) {
+      if (pIdx < p.length && (p[pIdx] === "?" || p[pIdx] === s[sIdx])) {
+        // Advance both pointers when (str[sIdx] == pattern[pIdx]) or (pattern[pIdx] == '?')
+        sIdx++;
+        pIdx++;
+      } else if (pIdx < p.length && p[pIdx] === "*") {
+        // '*' found, only advance pattern pointer
+        starIdx = pIdx;
+        sTmpIdx = sIdx;
+        pIdx++;
+      } else if (starIdx === -1) {
+        // Current characters didn't match, and no '*' was found in pattern
+        return false;
+      } else {
+        // Last pattern pointer was '*', advance string pointer
+        pIdx = starIdx + 1;
+        sIdx = sTmpIdx + 1;
+        sTmpIdx = sIdx;
+      }
+    }
+
+    
+    while (pIdx < p.length && p[pIdx] === "*") {
+      pIdx++;
+    }
+
+    return pIdx === p.length;
+  }
+
+  return match(s, p);
+};
+
+module.exports = decodeTheRing;
